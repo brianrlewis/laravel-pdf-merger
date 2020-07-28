771b2434b5253d2a2cd61bdec46e3c921787fe55
Brian Lewis <brlewis23@gmail.com>
2020-07-28 13:19:48 -0700

    Add optional feature to define temp storage path

--- src/PDFMerger.php
+++ src/PDFMerger.php
@@ -118,7 +118,7 @@ class PDFMerger {
      * @return void
      */
     public function addPDFString($string, $pages = 'all', $orientation = null){
-        $filePath = storage_path('tmp/'.Str::random(16).'.pdf');
+        $filePath = $this->getPath('tmp/'.Str::random(16).'.pdf');
         $this->filesystem->put($filePath, $string);
         $this->tmpFiles->push($filePath);
         return $this->addPathToPDF($filePath, $pages, $orientation);
@@ -209,7 +209,7 @@ class PDFMerger {
       preg_match_all('!\d+!', $first_line, $matches);
       $pdfversion = implode('.', $matches[0]);
       if($pdfversion > "1.4"){
-        $newFilePath = storage_path('tmp/' . Str::random(16) . '.pdf');
+        $newFilePath = $this->getPath('tmp/' . Str::random(16) . '.pdf');
         //execute shell script that converts PDF to correct version and saves it to tmp folder
         shell_exec('gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="'. $newFilePath . '" "' . $filePath . '"');
         $this->tmpFiles->push($newFilePath);
@@ -226,8 +226,20 @@ class PDFMerger {
      */
     protected function createDirectoryForTemporaryFiles(): void
     {
-        if (! $this->filesystem->isDirectory(storage_path('tmp'))) {
-            $this->filesystem->makeDirectory(storage_path('tmp'));
+        if (! $this->filesystem->isDirectory($this->getPath('tmp'))) {
+            $this->filesystem->makeDirectory($this->getPath('tmp'));
         }
+    }
+
+    protected function getPath($path)
+    {
+      if ($basePath = config('pdfmerger.temp_file_path')) {
+        if (!Str::endsWith($basePath, '/')) {
+          $basePath .= '/';
+        }
+        return $basePath.$path;
+      } else {
+        return storage_path($path);
+      }
     }
 }
